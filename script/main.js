7634b8afc7030c095a2d4b8c70be1869b8ddb1fb
Todd Anderson <bustardcelly@gmail.com>
2024-08-12 10:34:27 +0200

    feature: live playback with interstitial.

--- script/main.js
+++ script/main.js
@@ -1,17 +1,61 @@
-/*global red5prosdk*/
-const { setLogLevel, WHEPClient } = red5prosdk;
+/*
+Copyright © 2015 Infrared5, Inc. All rights reserved.
 
+The accompanying code comprising examples for use solely in conjunction with Red5 Pro (the "Example Code")
+is  licensed  to  you  by  Infrared5  Inc.  in  consideration  of  your  agreement  to  the  following
+license terms  and  conditions.  Access,  use,  modification,  or  redistribution  of  the  accompanying
+code  constitutes your acceptance of the following license terms and conditions.
+
+Permission is hereby granted, free of charge, to you to use the Example Code and associated documentation
+files (collectively, the "Software") without restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The Software shall be used solely in conjunction with Red5 Pro. Red5 Pro is licensed under a separate end
+user  license  agreement  (the  "EULA"),  which  must  be  executed  with  Infrared5,  Inc.
+An  example  of  the EULA can be found on our website at: https://account.red5.net/assets/LICENSE.txt.
+
+The above copyright notice and this license shall be included in all copies or portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,  INCLUDING  BUT
+NOT  LIMITED  TO  THE  WARRANTIES  OF  MERCHANTABILITY, FITNESS  FOR  A  PARTICULAR  PURPOSE  AND
+NONINFRINGEMENT.   IN  NO  EVENT  SHALL INFRARED5, INC. BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN  AN  ACTION  OF  CONTRACT,  TORT  OR  OTHERWISE,  ARISING  FROM,  OUT  OF  OR  IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+/*global red5prosdk*/
+import { query } from "./url-util.js";
+import AdServiceImpl from "./ad-service.js";
+import InterstitialServiceImpl from "./interstitial-service.js";
+
+const { setLogLevel, WHEPClient } = red5prosdk;
 setLogLevel("debug");
 
 const NAME = "[Red5:IBC]";
-
 const SourceSection = Object.freeze({
 	SOURCES: "sources",
 	CLIPS: "clips",
 });
+const ipv4Pattern =
+	/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+const hostIsIPv4 = (host) => ipv4Pattern.test(host);
+
+const { host, app, streamName, get } = query();
+const service = new InterstitialServiceImpl(host, app, streamName);
+const adService = new AdServiceImpl(app);
+
+const baseConfiguration = {
+	host,
+	app,
+	protocol: hostIsIPv4(host) ? "ws" : "wss",
+	port: hostIsIPv4(host) ? "5080" : 443,
+};
 
 const sourceButtons = Array.from(
 	document.querySelectorAll(".source-selector_item"),
+);
+const sourceContainers = Array.from(
+	document.querySelectorAll(".source-container_source"),
 );
 sourceButtons.forEach((button) => {
 	button.addEventListener("click", (event) => {
@@ -24,13 +68,56 @@ sourceButtons.forEach((button) => {
 const selectSourceSection = (selection) => {
 	switch (selection) {
 		case SourceSection.SOURCES:
-			console.log("SOURCES");
+			sourceButtons
+				.find((button) => button.dataset.selection === SourceSection.SOURCES)
+				.classList.add("source-selector_selected");
+			sourceButtons
+				.find((button) => button.dataset.selection === SourceSection.CLIPS)
+				.classList.remove("source-selector_selected");
+			sourceContainers
+				.find((container) => container.dataset.source === SourceSection.SOURCES)
+				.classList.remove("hidden");
+			sourceContainers
+				.find((container) => container.dataset.source === SourceSection.CLIPS)
+				.classList.add("hidden");
+			// TODO: Load sources
 			break;
 		case SourceSection.CLIPS:
-			console.log("CLIPS");
+			sourceButtons
+				.find((button) => button.dataset.selection === SourceSection.SOURCES)
+				.classList.remove("source-selector_selected");
+			sourceButtons
+				.find((button) => button.dataset.selection === SourceSection.CLIPS)
+				.classList.add("source-selector_selected");
+			sourceContainers
+				.find((container) => container.dataset.source === SourceSection.SOURCES)
+				.classList.add("hidden");
+			sourceContainers
+				.find((container) => container.dataset.source === SourceSection.CLIPS)
+				.classList.remove("hidden");
+			// TODO: Load clips
 			break;
 		default:
 			console.error("Invalid selection");
 			break;
 	}
 };
+
+const startLiveStream = async () => {
+	try {
+		const { WHEPClient } = red5prosdk;
+		const configuration = {
+			...baseConfiguration,
+			streamName,
+			mediaElementId: "live-video_video_element",
+		};
+		const subscriber = new WHEPClient();
+		await subscriber.init(configuration);
+		await subscriber.subscribe();
+	} catch (error) {
+		console.error(error);
+		alert(`Failed to start live stream: ${error.message}`);
+	}
+};
+
+startLiveStream();
---
+++ script/url-util.js
@@ -0,0 +1,72 @@
+/*
+Copyright © 2015 Infrared5, Inc. All rights reserved.
+
+The accompanying code comprising examples for use solely in conjunction with Red5 Pro (the "Example Code")
+is  licensed  to  you  by  Infrared5  Inc.  in  consideration  of  your  agreement  to  the  following
+license terms  and  conditions.  Access,  use,  modification,  or  redistribution  of  the  accompanying
+code  constitutes your acceptance of the following license terms and conditions.
+
+Permission is hereby granted, free of charge, to you to use the Example Code and associated documentation
+files (collectively, the "Software") without restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The Software shall be used solely in conjunction with Red5 Pro. Red5 Pro is licensed under a separate end
+user  license  agreement  (the  "EULA"),  which  must  be  executed  with  Infrared5,  Inc.
+An  example  of  the EULA can be found on our website at: https://account.red5.net/assets/LICENSE.txt.
+
+The above copyright notice and this license shall be included in all copies or portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,  INCLUDING  BUT
+NOT  LIMITED  TO  THE  WARRANTIES  OF  MERCHANTABILITY, FITNESS  FOR  A  PARTICULAR  PURPOSE  AND
+NONINFRINGEMENT.   IN  NO  EVENT  SHALL INFRARED5, INC. BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN  AN  ACTION  OF  CONTRACT,  TORT  OR  OTHERWISE,  ARISING  FROM,  OUT  OF  OR  IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/**
+ * Utility for parsing query params.
+ */
+
+const paramExclude = [];
+
+/**
+ * Return flag indicating whether or not the query params include a host.
+ * @returns {boolean} Whether or not the query params include a host.
+ */
+export const hasHostDefined = () => {
+	const searchParams = new URLSearchParams(window.location.search);
+	return searchParams.has("host");
+};
+
+/**
+ * Utility for parsing query params.
+ * The following query parameters are supported:
+ * - host: The Red5 Pro Server hostname on which the live streams are located.
+ * - app: The app context on the Red5 Pro Server on which the live streams are located.
+ * - streamName: The name of the stream to broadcast on and subscribe to.
+ *
+ * Any other query parameters will be parsed as stream names and labels.
+ *
+ * @returns {object} Object containing the query params.
+ */
+export const query = () => {
+	const searchParams = new URLSearchParams(window.location.search);
+	let host = searchParams.get("host")
+		? decodeURIComponent(searchParams.get("host"))
+		: undefined;
+	let app = searchParams.get("app")
+		? decodeURIComponent(searchParams.get("app"))
+		: "live";
+	let streamName = searchParams.get("streamName")
+		? decodeURIComponent(searchParams.get("streamName"))
+		: "stream1";
+	return {
+		host,
+		app,
+		streamName,
+		get: (key) => {
+			return searchParams.get(key);
+		},
+	};
+};
