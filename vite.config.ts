c9b395c5edcafef83b69fb2a518bcf5c6ab596f0
Todd Anderson <bustardcelly@gmail.com>
2025-08-15 10:30:57 -0400

    fix: build and versioning.

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  base: '',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json']
+  }
+})
